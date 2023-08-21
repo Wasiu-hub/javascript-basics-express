22dafdb6ba17bd567f613ed2c11ff7dcd438db8e
Wasiu-hub <contactbow@yahoo.com>
2023-08-21 22:37:21 +0100

    test for sayHello function passed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const { sayHello } = require('./lib/strings');
 
 const app = express();
 
 // strings endpoints
-app.get('/strings/hello/world', (req, res) => {
-  res.status(200).json({ result: 'Hello, world!' });
+// app.get('/strings/hello/world', (req, res) => {
+//   res.status(200).json({ result: 'Hello, world!' });
+// });
+
+app.get('/strings/hello/:string', (req, res) => {
+  res.json({ result: sayHello(req.params.string) });
 });
 
 
