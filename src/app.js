1b728ad04f85f357964d900e1749cbc3807f8a93
Wasiu-hub <contactbow@yahoo.com>
2023-08-21 22:30:26 +0100

    test for 'return Hello, World' passed

--- src/app.js
+++ src/app.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+// strings endpoints
+app.get('/strings/hello/world', (req, res) => {
+  res.status(200).json({ result: 'Hello, world!' });
+});
+
+
 module.exports = app;
