1da271455270dd29ede8df44caab9e64989d6739
Wasiu-hub <contactbow@yahoo.com>
2023-08-22 03:08:52 +0100

    test for multiply method passed

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { sayHello, uppercase, lowercase, firstCharacter, firstCharacters } = require('./lib/strings');
 
-const { add, subtract } = require('./lib/numbers');
+const { add, subtract, multiply } = require('./lib/numbers');
 
 const app = express();
+app.use(express.json());
 
 // strings endpoints
 // app.get('/strings/hello/world', (req, res) => {
@@ -55,6 +56,20 @@ app.get('/numbers/subtract/:a/from/:b', (req, res) => {
   }
   res.status(200).json({ result: subtract(b, a) });
 });
+
+app.post('/numbers/multiply', (req, res) => {
+
+  const { a, b } = req.body;
+  if (a === undefined || b === undefined ) {
+    return res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+    
+  } else if (isNaN(a) || isNaN(b)){
+    return res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  } 
+
+  res.status(200).json({ result: multiply(a,b) });
+});
+
 
 
 module.exports = app;
